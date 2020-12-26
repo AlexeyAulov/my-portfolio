c3aeb18f835953051d1bb586365a772b492a35d3
Alex-Pro-commit <73656348+Alex-Pro-commit@users.noreply.github.com>
2020-12-26 11:26:34 -0500

    Responsive now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,29 +8,29 @@ export default function NavBar()
 return (
 <header className="bg-green-400">
 <div className="container mx-auto flex justify-between">
-<nav className="flex">
+<nav className="inline-flex ">
 
-<NavLink to="/"exact 
-activeClassName="text-yellow-200 "
-className="inflex-flex items-center py-8 px-3 mr-4 text-black-300 hover:text-yellow-200 text-5xl " >
-Home
-</NavLink>
+    <NavLink to="/"exact 
+    activeClassName="text-yellow-200 "
+    className=" flex-initial items-center py-9 px-3 mr-4 text-black-300 hover:text-yellow-200 text-4xl " >
+    Home
+    </NavLink>
 
-<NavLink to="/project" 
-activeClassName="text-yellow-200"
-className="inline-flex items-center py-3 px-3  text-4xl rounded text-black hover:text-yellow-200">
-Projects
-</NavLink>
+    <NavLink to="/project" 
+    activeClassName="text-yellow-200"
+    className=" flex-initial items-center py-10 px-3  text-3xl rounded text-black hover:text-yellow-200">
+    Projects
+    </NavLink>
 
 
 
 </nav>
-<div className="flex sm:inline-flex md:flex lg:flex xl:flex ... py-3 px-3 my-6 gap-2">
-<SocialIcon url="https://www.linkedin.com/in/alexey-aulov/" ClassName="mr-4" target="_blank" fgColor="#fff" style ={{height:55,width:55}}/>
+<div className="flex-initial sm:inline-flex md:flex lg:flex xl:flex ... py-3 px-3 my-6 gap-2">
+<SocialIcon url="https://www.linkedin.com/in/alexey-aulov/" ClassName="mr-4" target="_blank" fgColor="#fff" style ={{height:45,width:45}}/>
   
-<SocialIcon url="https://github.com/AlexeyAulov" ClassName="mr-4" target="_blank" fgColor="#fff" style ={{height:55,width:55}}/>
+<SocialIcon url="https://github.com/AlexeyAulov" ClassName="mr-4" target="_blank" fgColor="#fff" style ={{height:45,width:45}}/>
 
-<a  href="https://github.com/AlexeyAulov/Resume/blob/main/Alexey%20Aulov.pdf" target="_blank" rel="noreferrer"><GoFile ClassName="mr-4"  fgColor="#fff" style ={{height:55,width:55}}/></a>
+<a  href="https://github.com/AlexeyAulov/Resume/blob/main/Alexey%20Aulov.pdf" target="_blank" rel="noreferrer"><GoFile ClassName="mr-4"  fgColor="#fff" style ={{height:45,width:45}}/></a>
 
 </div>
 </div>   
--- src/components/Project.js
+++ src/components/Project.js
@@ -15,9 +15,9 @@ export default function Project()
     
 return(
         <main className="bg-yellow-200 min-h-screen p-12">
-                <section className="container mx-auto">
+                <section className="container  mx-auto">
                     <h1 className="text-5xl flex justify-center pb-4">My Projects</h1>
-                    <section className="grid grid-cols-2 gap-8">    
+                    <section className="grid lg:grid-cols-2 md:grid-cols-2 md:grid-cols-1 gap-8">    
                         {projectData && projectData.map((project,index)=>(
                             <article className="relative rounded-lg shadow-xl bg-white p-16">
                                 <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text:yellow-700">
